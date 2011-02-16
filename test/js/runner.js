a6a477b7f72dd21b7ade2f3afcc72b6e82fb26ed
Michael Brooks <michael@michaelbrooks.ca>
2011-02-16 15:33:40 -0800

    Move jQTouch test suite to jqtouch.html

--- test/js/runner.js
+++ test/js/runner.js
@@ -1,6 +1,31 @@
 (function() {
+
+    window.addEventListener('load', function() {
+        if (typeof window.iframe === 'undefined') {
+            console.log('hmm');
+            window.iframe = document.createElement('iframe');
+            iframe.setAttribute('src',         '../vendor/jqtouch/demos/main/index.html');
+            iframe.setAttribute('id',          'app');
+            iframe.setAttribute('name',        'app');
+            iframe.setAttribute('width',       '320');
+            iframe.setAttribute('height',      '480');
+            iframe.setAttribute('scrolling',   'auto');
+            iframe.setAttribute('frameborder', '0');
+            // iframe.setAttribute('style', 'visibility:hidden;position:absolute;top:0px;left:0px;z-index:-1;');
+        }
+
+        window.iframe.onload = function() {
+            var e = document.createEvent('Events');
+            e.initEvent('appready');
+            document.dispatchEvent(e);
+        };
+
+        document.body.appendChild(iframe);
+    }, false);
+
     document.addEventListener('appready', function() {
         please.config.window = document.getElementById('app').contentWindow;
         please.test();
     }, false);
+
 })();
