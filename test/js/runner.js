97f08ec91e9f5c10f90d44f08a2bfeb579a9e02c
Michael Brooks <michael@michaelbrooks.ca>
2011-02-18 15:13:40 -0800

    Comment the test runner.

--- test/js/runner.js
+++ test/js/runner.js
@@ -1,9 +1,22 @@
+//
+// Simple Test Runner
+//
+// onLoad, an iFrame loads the application to test.
+// You can control what application is loaded by defining window.iframe
+//
+// After the application has loaded in the iFrame, an 'appready' event is fired.
+//
+// Dominator begins to run when the 'appready' event is fired.
+//
 (function() {
 
+    //
+    // Load your application to test.
+    //
     window.addEventListener('load', function() {
         if (typeof window.iframe === 'undefined') {
             window.iframe = document.createElement('iframe');
-            iframe.setAttribute('src',         '../vendor/jqtouch/demos/main/index.html');
+            iframe.setAttribute('src',         'default/index.html');
             iframe.setAttribute('id',          'app');
             iframe.setAttribute('name',        'app');
             iframe.setAttribute('width',       '320');
@@ -13,6 +26,9 @@
             // iframe.setAttribute('style', 'visibility:hidden;position:absolute;top:0px;left:0px;z-index:-1;');
         }
 
+        //
+        // Once the application is fully loaded, fire 'appready'
+        //
         window.iframe.onload = function() {
             var e = document.createEvent('Events');
             e.initEvent('appready');
@@ -22,10 +38,16 @@
         document.body.appendChild(iframe);
     }, false);
 
+    //
+    // Application has loaded. Run Dominator.
+    //
     document.addEventListener('appready', function() {
         document.removeEventListener('appready', arguments.callee, false);
 
+        // Dominator tests are written against this.window.
+        // Here is where we define the handle to this.window.
         dominator.config.window = document.getElementById('app').contentWindow;
+
         dominator.test();
     }, false);
 
